d4f7ec40f8cad22228cdd268a75627bebb69aa63
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-03 15:27:34 +0000

    fourth

---
+++ getAndPrintHTML-options.js
@@ -0,0 +1,31 @@
+var https = require('https');
+var fs = require('fs');
+
+//console log each chunk of data as it is recieved
+// need to invoke the function.
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+var htmlString = ""
+
+function getAndPrintHTML() {
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      htmlString += data;
+    });
+
+    response.on('end', function() {
+      console.log(htmlString, "\n", 'Stream complete.');
+    });
+  });
+}
+
+getAndPrintHTML()
+
